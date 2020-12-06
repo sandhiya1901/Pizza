40ad15c18ee149ccc298927730ee66186591d078
sandhiya.c <sandhiya.c@infinitisoftware.net>
2020-12-06 19:55:21 +0530

    changes

--- src/app/order-list/order-list.module.ts
+++ src/app/order-list/order-list.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { OrderListRoutingModule } from './order-list-routing.module';
 import { OrdersComponent } from './orders/orders.component';
 import { SharedModule } from '../shared/shared.module';
-import { ViewOrderModule } from '../view-order/view-order.module';
 
 
 @NgModule({
--- src/app/view-order/view-order.module.ts
+++ src/app/view-order/view-order.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ViewOrderRoutingModule } from './view-order-routing.module';
 import { ViewComponent } from './view/view.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ViewComponent],
   imports: [
     CommonModule,
-    ViewOrderRoutingModule,
-    SharedModule
+    ViewOrderRoutingModule
   ]
 })
 export class ViewOrderModule { }
